85dc877aedc118b555ae8f5b0abed05786f4da6d
Kishan Jasani <kishanjasani007@yahoo.in>
2020-11-26 11:47:13 +0530

    Add basic files

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Theme Functions.
+ *
+ * @package Aquila_Portfolio
+ */
+
+/**
+ * Register styles and scripts.
+ *
+ * @action wp_enqueue_scripts
+ */
+function aquila_portfolio_enqueue_script() {
+	wp_enqueue_script( 'jquery' );
+}
+
+add_action( 'wp_enqueue_scripts', 'aquila_portfolio_enqueue_script'  );
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package Aquila_Portfolio
+ */
+
+?>
+	<div class="content">
+		Content
+	</div>
+<?php
