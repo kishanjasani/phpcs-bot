90bd53f21a17331332ca38feb10c1fba6f1b706e
Kishan Jasani <kishanjasani007@yahoo.in>
2020-11-26 12:06:13 +0530

    Add more phpcs error

--- functions.php
+++ functions.php
@@ -14,4 +14,4 @@ function aquila_portfolio_enqueue_script() {
 	wp_enqueue_script( 'jquery'  );
 }
 
-add_action( 'wp_enqueue_scripts', 'aquila_portfolio_enqueue_script'  );
+add_action( 'wp_enqueue_scripts', 'aquila_portfolio_enqueue_script'   );
